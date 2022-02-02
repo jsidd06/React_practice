fc978d3653721b594d1abdb1f996d5569ea08aac
siddharth <jsidd06@gmail.com>
2022-02-02 11:51:28 +0530

    practice module 1 program percentage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Container, Form, Input, Label } from 'reactstrap';
 
 function App() {
+    const [count,setCount] = useState(0);
+    const [countCorrect,setCountCorrect] = useState(0);
+    const [percentage,setPercentage] = useState(0);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const formValid = +count >= 0 && +countCorrect;
+        if(!formValid) {
+            return;
+        }
+        Math.floor(+count/+countCorrect*100) > 100 ? setPercentage(100) : setPercentage(Math.floor(+count/+countCorrect*100));
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+        <Form onSubmit={submitHandler}>
+            <Label>Type you get number</Label> 
+            <Input placeholder="enter the number" value={count} onChange={(e) => setCount(e.target.value)}/>
+            <Label>Type total of number</Label>
+            <Input placeholder="enter the number" value={countCorrect} onChange={(e) => setCountCorrect(e.target.value)} />
+            <Button type="submit">Submit</Button> 
+        </Form>
+        <h1>percentage:-{percentage}%</h1>
+        </Container>
+  )
 }
 
 export default App;
